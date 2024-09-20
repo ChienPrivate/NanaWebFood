2c7eeb16412cf2245d08c34810c722cb6f1b7b44
cdmasd <tienpdps28044@fpt.edu.vn>
2024-09-20 07:45:51 +0700

    modify solution

--- NanaFoodProject/NanaFoodWeb/NanaFoodApi/obj/Debug/net8.0/NanaFoodApi.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodApi/obj/Debug/net8.0/NanaFoodApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b8b7f4c9d68ca527c93a9180542d35a5bb2641")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
