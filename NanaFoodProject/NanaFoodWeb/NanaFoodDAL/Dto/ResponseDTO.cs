e854b939364ec88bed5f08c0bff044992df4fef4
cdmasd <tienpdps28044@fpt.edu.vn>
2024-09-20 09:23:20 +0700

    FEAT ICategoryService/CategoryService Category API

---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodDAL/Dto/ResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanaFoodDAL.Dto
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+    }
+}
