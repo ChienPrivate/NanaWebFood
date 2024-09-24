7a476b224f28ef2e16ad617e709ac807ac85f6c0
TramPham001 <tramptnps27441@fpt.edu.vn>
2024-09-24 21:20:44 +0700

    commit dashboard

--- NanaFoodProject/NanaFoodWeb/NanaFoodApi/obj/Debug/net8.0/NanaFoodApi.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodApi/obj/Debug/net8.0/NanaFoodApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+754c03cb76d2f1dcda3c469108ed2ba54ca9847b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b482cd2b5959125c7a24fbc1dbce09f3b3c138")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
