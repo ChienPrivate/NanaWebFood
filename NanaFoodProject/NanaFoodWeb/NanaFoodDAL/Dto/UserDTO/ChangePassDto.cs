e866cfbde53dfa3e1039af5c496e0033eb44ef54
cdmasd <tienpdps28044@fpt.edu.vn>
2024-09-28 10:43:14 +0700

    FEAT : Change Password

---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodDAL/Dto/UserDTO/ChangePassDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanaFoodDAL.Dto.UserDTO
+{
+    public class ChangePassDto
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu không được rỗng")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$",
+        ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự, bao gồm 1 chữ in hoa, 1 chữ thường và 1 số.")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Mật khẩu nhập lại không trùng khớp.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
