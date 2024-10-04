b623b86474195f4c5d6b845e297b47c96ffa4739
hoang truong <hoangtmps35865@fpt.edu.vn>
2024-10-04 20:30:00 +0700

    Login & regiter page

---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NanaFoodWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register() 
+        {
+            return View();
+        }
+    }
+}
