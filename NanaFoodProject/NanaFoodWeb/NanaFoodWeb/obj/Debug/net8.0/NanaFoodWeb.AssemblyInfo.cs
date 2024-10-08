8011627d10dc9e7486c953fab21d7d5f3fc8b80e
TramPham001 <tramptnps27441@fpt.edu.vn>
2024-10-08 10:18:09 +0700

    commit toast

--- NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b266d031a2ce3926253324ab68b00dbf3c5939ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ebe841894c080d80574fe8c7a0282d81f823082")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
