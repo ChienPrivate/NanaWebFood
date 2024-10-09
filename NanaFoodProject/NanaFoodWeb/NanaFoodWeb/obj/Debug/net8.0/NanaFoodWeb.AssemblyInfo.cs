fc9765bdb4eb59ea7b7b35782df35e41fa7d59df
cdmasd <tienpdps28044@fpt.edu.vn>
2024-10-09 10:12:19 +0700

    Add folder

--- NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce803b407c88d1e061417ad47f165066c66773ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3320fb1cf832db9fd54cea4b4b29e2ef42c210f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
