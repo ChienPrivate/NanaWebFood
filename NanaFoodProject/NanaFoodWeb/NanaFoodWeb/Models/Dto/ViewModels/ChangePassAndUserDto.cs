b565ba394d2ef9aba97aa1c15b1c30f5c1c8e5af
cdmasd <tienpdps28044@fpt.edu.vn>
2024-10-16 12:38:30 +0700

    Changepass consume

---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/Models/Dto/ViewModels/ChangePassAndUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NanaFoodWeb.Models.Dto.ViewModels
+{
+    public class ChangePassAndUserDto
+    {
+        public UserDto UserDto { get; set; }
+        public ChangePasswordDto? changepass {get; set;}
+    }
+}
