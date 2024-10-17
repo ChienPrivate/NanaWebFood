6960c8c6484a42f2b6467099368aff17a6dc6074
Cee <chiennguyen2841998@gmail.com>
2024-10-17 15:37:31 +0700

    update validate file for pull and drop image in categories

--- NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06764c1540f11ff160fd66056ff0d9b58f79aff1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789312089b9741da9a0108a71934c3c87c7455c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
