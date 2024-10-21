6f8c6abbab233543b4e9ef6ff60ba56ad0b243f5
Admin <truongtvps31921@fpt.edu.vn>
2024-10-21 22:48:19 +0700

    layoutHome

--- NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8983c1e04b39d09bf38a4120f95089af88ad82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f2e68d2e3235ef450853a45648e7b91735196a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
