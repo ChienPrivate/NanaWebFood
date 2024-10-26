d0db466af27304be204ec34fb293cd73fb9fca74
Admin <truongtvps31921@fpt.edu.vn>
2024-10-26 21:18:29 +0700

    add coupon to payment

--- NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/obj/Debug/net8.0/NanaFoodWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373540339ec578fb69714ad798c3d0f3473b2b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4e752268769398264530b16c5b28cf60bd4e34")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaFoodWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
