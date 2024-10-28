a0e98ab92c72358e416c351cd0ac0e9276450817
cdmasd <tienpdps28044@fpt.edu.vn>
2024-10-28 10:35:44 +0700

    ChangePassword (Ajax)

--- NanaFoodProject/NanaFoodWeb/NanaFoodDAL/Dto/UserDTO/ChangePassDto.cs
+++ NanaFoodProject/NanaFoodWeb/NanaFoodDAL/Dto/UserDTO/ChangePassDto.cs
@@ -12,14 +12,14 @@ namespace NanaFoodDAL.Dto.UserDTO
         /// <summary>
         /// Mật khẩu hiện tại
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "Không được rỗng")]
         [DataType(DataType.Password)]
         public string OldPassword { get; set; }
 
         /// <summary>
         /// Mật khẩu muốn thay đổi
         /// </summary>
-        [Required(ErrorMessage = "Mật khẩu không được rỗng")]
+        [Required(ErrorMessage = "Không được rỗng")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$",
         ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự, bao gồm 1 chữ in hoa, 1 chữ thường và 1 số.")]
         public string NewPassword { get; set; }
@@ -27,6 +27,7 @@ namespace NanaFoodDAL.Dto.UserDTO
         /// <summary>
         /// Xác nhận lại mật khẩu cần thay đổi
         /// </summary>
+        [Required(ErrorMessage = "Không được rỗng")]
         [DataType(DataType.Password)]
         [Compare("NewPassword", ErrorMessage = "Mật khẩu nhập lại không trùng khớp.")]
         public string ConfirmPassword { get; set; }
