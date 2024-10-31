e94e9ccc291d5c7134b2e9c022960a21fc0796a6
Cee <chiennguyen2841998@gmail.com>
2024-10-31 08:35:43 +0700

    add review section into CDetails

---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodDAL/Dto/UserReviewDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanaFoodDAL.Dto
+{
+    public class UserReviewDto
+    {
+        public string ReviewId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string AvatarUrl { get; set; }
+        public string? Comment { get; set; }
+        public double? Rating { get; set; }
+        public DateTime ReviewedDate { get; set; }
+    }
+}
---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodWeb/Models/Dto/UserReviewDto.cs
@@ -0,0 +1,14 @@
+﻿namespace NanaFoodWeb.Models.Dto
+{
+    public class UserReviewDto
+    {
+        public string ReviewId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string AvatarUrl { get; set; }
+        public string? Comment { get; set; }
+        public double? Rating { get; set; }
+        public DateTime ReviewedDate { get; set; }
+    }
+}
