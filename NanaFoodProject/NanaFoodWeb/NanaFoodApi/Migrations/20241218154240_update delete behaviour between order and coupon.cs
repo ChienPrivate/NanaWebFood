8823f6d65c72fc616fd843952bd2aedacf277020
Cee <chiennguyen2841998@gmail.com>
2024-12-19 10:45:40 +0700

    add 2 page to admin layout and update loading progress in order

---
+++ NanaFoodProject/NanaFoodWeb/NanaFoodApi/Migrations/20241218154240_update delete behaviour between order and coupon.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NanaFoodApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedeletebehaviourbetweenorderandcoupon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Thay đổi hành vi khóa ngoại của CouponCode trong Order
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Coupon_CouponCode",  // Tên khóa ngoại (có thể thay đổi tùy theo tên đã đặt)
+                table: "Order");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Coupon_CouponCode",
+                table: "Order",
+                column: "CouponCode",
+                principalTable: "Coupon",
+                principalColumn: "CouponCode",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
